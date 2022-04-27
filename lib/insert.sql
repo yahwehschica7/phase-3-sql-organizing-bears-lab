39ce38e116ad64564ddcef5a526effcac72ff610
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-27 14:34:17 -0400

    commit tree a29f94fde680340bdcaaa4d71a588c3f3081b9c5

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 10, "M", "pink", "persnickety", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 6, "M", "pink", "persnickety", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 20, "F", "pink", "persnickety", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 4, "M", "pink", "persnickety", 1);
+INSERT INTO bears VALUES (5,"Melissa", 12, "F", "pink", "persnickety", 1);
+INSERT INTO bears VALUES (6,"Grinch", 8, "M", "pink", "persnickety", 1);
+INSERT INTO bears VALUES (7,"Wendy", 25, "F", "pink", "persnickety", 0);
+INSERT INTO bears VALUES (8,null, 4, "M", "pink", "persnickety", 0);
+
+
+
+
+
